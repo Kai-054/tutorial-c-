f25115cb1711c390e5d446e3758436c712d15923
khai Nguyen <khainguyentruong@gmail.com>
2025-07-07 19:23:13 +0700

    update ex 10-15

--- buoi1/1.cpp
+++ buoi1/1.cpp
@@ -79,17 +79,98 @@ void bai9() {
     long long sum = (n*n*(n*n+1))/4;
     cout << sum << endl;
 }
-void bai10() {}
-void bai11() {}
-void bai12() {}
-void bai13() {}
-void bai14() {}
-void bai15() {}
+
+void value_moth() {
+    int n;
+    cin >> n;
+    if ((n==1)||(n==2)||(n==3)||(n==4)||(n==5)) {
+        cout << "value moth 31 days" << endl;
+    }
+    else if ((n==6)||(n==7)||(n==8)||(n==9)||(n==10)) {
+        cout << "value moth 63 days" << endl;
+    }
+    else {
+            cout << "value moth 95 days" << endl;
+        }
+}
+
+void structure_case() {
+    int a ;
+    cin >> a;
+    switch (a) {
+        case 1:
+            cout << "value moth 1" << endl;
+            break;
+            case 2:
+            cout << "value moth 2" << endl;
+            break;
+            case 3:
+            bai1();
+            break;
+            case 4: case 5: case 6:
+            cout << "value moth 3" << endl;
+            break;
+            default:
+            cout << "error ! \n" << endl;
+    }
+}
+
+void bai10() {
+    int n;
+    cin >> n;
+    double rs  = 1 - 1.0/(n+1) ;
+    cout << fixed << setprecision(2) << rs << endl;
+}
+void bai11() {
+    int n;
+    cin >> n;
+    long long sum = 2.0*n*(n+1)/2;
+    cout << sum << endl;
+}
+void bai12() {
+    long long n;
+    cin >> n;
+    if (n%2==0) {
+        long long sum = n /2;
+        cout << sum << endl;
+    }
+    else {
+        long long sum = (-n-1)/2;
+        cout << sum << endl;
+    }
+}
+void bai13() {
+    int a,b ;
+    cin >> a >> b ;
+    int device = a/b * b;
+    cout << device << endl;
+}
+void bai14() {
+    int a ,b ;
+    cin >> a >> b ;
+    if (a%2 !=0) {
+        int device = ((a / b) + 1) * b;
+        cout << device << endl;
+    }
+    else {
+        int device = a/b * b;
+        cout << device << endl;
+    }
+}
+void bai15() {
+    int a;
+    bool l1;
+    cin >> a;
+    l1 = a%3==0 && a%5==0 ;
+    cout << l1 << endl;
+
+}
 void bai16() {}
 void bai17() {}
 void bai18() {}
 void bai19() {}
 void bai20() {}
+
 void bai21() {}
 void bai22() {}
 void bai23() {}
@@ -109,6 +190,9 @@ int main() {
     // bai3();
     // bai4();
     // bai5();
-    bai9();
+    // bai9();
+    // value_moth();
+    // structure_case();
+    bai15();
     return 0;
 }
